c456ee62104533fd8a963f2668f0ac0767caf179
Michael Mushrush <mushrushm@gmail.com>
2022-09-04 12:46:48 -0400

    Spawner

--- Assets/Scripts/Scriptables/SpawnerScriptableObject.cs
+++ Assets/Scripts/Scriptables/SpawnerScriptableObject.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum EnemyTypes { FROG }
+public enum EnemyTypes { FROG, RACCOON, MOSQUITO, SQUIRREL, OWL }
 
 [CreateAssetMenu(fileName = "New Enemy",menuName = "Enemy")]
 public class SpawnerScriptableObject : ScriptableObject
 {
     //Script hasn't been implemented for enemy yet so I'm just using GO here.
     public GameObject frogEnemy;
+    public GameObject raccoonEnemy;
+    public GameObject mosquitoEnemy;
+    public GameObject squirrelEnemy;
+    public GameObject owlEnemy;
 
     public GameObject GetEnemyInstance(EnemyTypes enemyType)
     {
@@ -18,6 +22,18 @@ public class SpawnerScriptableObject : ScriptableObject
         {
             case EnemyTypes.FROG:
                 enemy = frogEnemy;
+                break;
+            case EnemyTypes.RACCOON:
+                enemy = raccoonEnemy;
+                break;
+            case EnemyTypes.MOSQUITO:
+                enemy = mosquitoEnemy;
+                break;
+            case EnemyTypes.SQUIRREL:
+                enemy = squirrelEnemy;
+                break;
+            case EnemyTypes.OWL:
+                enemy = owlEnemy;
                 break;
         }
 
